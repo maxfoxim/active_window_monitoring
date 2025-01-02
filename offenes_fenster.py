2e5f008c69495fee96bd40c8d957bd21ed68a51f
stephan.fuchs <stephan.fuchs@robotron.de>
2025-01-02 13:17:06 +0100

    viele Verbesserungen

--- offenes_fenster.py
+++ offenes_fenster.py
@@ -12,20 +12,21 @@ TODO:
 
 """
 
-ANZAHL_ITERATIONEN=6 #* 10^10
+ANZAHL_ITERATIONEN= 10**10
 ALLE_SEKUNDEN_SPEICHERN_JSON=5
-SCHLAFZEIT=2 #
-ALLE_SEKUNDEN_SPEICHERN_BARCHART=60*5
+SCHLAFZEIT=2 # Abstand Messung
+ALLE_SEKUNDEN_SPEICHERN_BARCHART=1#60*5
 
-Programme = ["Visual Studio Code", "VMware Remote Console",
-             "DBeaver", "Mozilla Firefox", "Rainbow","Windows PowerShell","Outlook"]
+Programme = ["Visual Studio Code", "VMware Remote Console","Excel",
+             "DBeaver", "Mozilla Firefox", "Rainbow","Windows PowerShell",
+             "Outlook","MaInSpec Validator","Zeiss Configuration Creator", 
+             "OneNote","Microsoft​ Edge"]
 
 def getForegroundWindowTitle() -> Optional[str]:
     hWnd = windll.user32.GetForegroundWindow()
     length = windll.user32.GetWindowTextLengthW(hWnd)
     buf = create_unicode_buffer(length + 1)
     windll.user32.GetWindowTextW(hWnd, buf, length + 1)
-    
     # 1-liner alternative: return buf.value if buf.value else None
     if buf.value:
         return buf.value
@@ -33,7 +34,9 @@ def getForegroundWindowTitle() -> Optional[str]:
         return None
 
 fenster={}
-anwendung={}
+anwendung={} 
+letztes_datum=""
+# mit 0 initalisiern
 for Programm in Programme:
     anwendung[Programm]=0
 
@@ -41,7 +44,25 @@ for i in range(ANZAHL_ITERATIONEN):
     print("------------------------------")
     aktuelles_fenster=getForegroundWindowTitle()
     
+    # Bereinigung:
     try:
+        aktuelles_fenster=aktuelles_fenster.replace("●","")
+    except:
+        pass
+    
+    now = datetime.now() # current date and time
+    aktuelles_datum = now.strftime("%m_%d")
+
+    # mit 0 initalisiern (auch bei neuem Tag)
+    if aktuelles_datum != letztes_datum:
+        for Programm in Programme:
+            anwendung[Programm]=0  
+        fenster={}
+        
+    letztes_datum = aktuelles_datum
+    
+    try:
+
         # Genaues Fenster
         if aktuelles_fenster in fenster:
             fenster[aktuelles_fenster]=fenster[aktuelles_fenster]+1
@@ -64,23 +85,60 @@ for i in range(ANZAHL_ITERATIONEN):
             gesamt={"Fenster":fenster,"Anwendung":anwendung}
             json.dump(gesamt, json_file, ensure_ascii = False, indent=4)
     
+
+    # Plotte Fenster 
     if i%ALLE_SEKUNDEN_SPEICHERN_BARCHART==0:
         x_arr,y_arr=[],[]
+        for key in fenster:
+            try:
+                x_arr.append(key)
+                y_arr.append(fenster[key])
+            except:
+                print("Nicht speicherbar")
+                
+        x_arr = [x for _,x in sorted(zip(y_arr,x_arr))]
+        y_arr = sorted(y_arr)
+            
+        print(x_arr,y_arr)
+        plt.clf()
+        plt.figure(figsize=(18,10))        
+        plt.barh(x_arr,y_arr)
+        for x,y in zip(x_arr,y_arr):
+            plt.text(y,x,y,fontsize=16,horizontalalignment='left',verticalalignment='center')
+        plt.title("Dauer Fenster")
+        plt.subplots_adjust(bottom=0.05,top=0.95,left=0.3,right=0.99)
+        plt.savefig("OffeneFenster_Details.png")
+        
+        plt.savefig("Offene_Fenster/Programm_"+aktuelles_datum+"_Details.png")
+        plt.clf()
+        plt.close()
+
+
+
+        # Plotte Anwendungen
+        x_arr,y_arr=[],[]
         for key in anwendung:
-            x_arr.append(key)
-            y_arr.append(anwendung[key])
+            if anwendung[key]>0:
+                x_arr.append(key)
+                y_arr.append(anwendung[key])
+        
+        x_arr = [x for _,x in sorted(zip(y_arr,x_arr))]
+        y_arr = sorted(y_arr)
+        
+        x_arr=x_arr[::-1]
+        y_arr=y_arr[::-1]
+
         plt.clf()
         plt.figure(figsize=(12,8))
         plt.bar(x_arr,y_arr)
         plt.xticks(rotation=45)
         #plt.grid()
+        for x,y in zip(x_arr,y_arr):
+            plt.text(x,y,y,fontsize=16,horizontalalignment='center',verticalalignment='bottom')
         plt.title("Dauer Programm")
         plt.subplots_adjust(bottom=0.2,left=0.05,right=0.95)
-        plt.savefig("Test.png")
+        plt.savefig("OffeneFenster.png")
         
-        now = datetime.now() # current date and time
-        folder = now.strftime("%m_%d")
-        plt.savefig("Offene_Fenster/Programm_"+folder+".png")
-
-print(anwendung)
-print(fenster)
+        plt.savefig("Offene_Fenster/Programm_"+aktuelles_datum+".png")
+        plt.clf()
+        plt.close()
